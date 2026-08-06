1ea0dc939f227242d758b78580e99154c81acc7a
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-06 13:11:52 +0330

    Update LocaleSwitcher.tsx

--- app/[locale]/LocaleSwitcher.tsx
+++ app/[locale]/LocaleSwitcher.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { usePathname, useRouter } from '@/i18n/navigation';
+import { usePathname, useRouter } from '../i18n/navigation';
 import { useLocale } from 'next-intl';
 
 export default function LocaleSwitcher() {
