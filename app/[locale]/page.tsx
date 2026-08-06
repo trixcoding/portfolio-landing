e2ab2d4965873c6f8a61f6866a57c78fd0d368c4
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-06 13:31:15 +0330

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import { useTranslations } from 'next-intl';
-import { Link } from '../../i18n/navigation';
+import { Link } from '../i18n/navigation';
 import LocaleSwitcher from './LocaleSwitcher';
 
 export default function Home() {
